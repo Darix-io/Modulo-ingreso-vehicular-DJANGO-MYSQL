c933a7d1d2ee13141ab0513d7dd3c5005181da32
Darix-io <rmoraj3@unemi.edu.ec>
2026-07-13 02:38:14 -0500

    Control de Acceso (Escaneo y Validación)

--- apps/qr/urls.py
+++ apps/qr/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import QrListView, renovar_qr_view
+from .views import QrListView, renovar_qr_view, validar_acceso_view
 
 app_name = 'qr'
 
 urlpatterns = [
     path('', QrListView.as_view(), name='lista'),
     path('renovar/<int:pk>/', renovar_qr_view, name='renovar'),
+    path('validar/', validar_acceso_view, name='validar'),
 ]
--- apps/qr/views.py
+++ apps/qr/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from django.shortcuts import render
+from django.utils import timezone
 from .models import CodigoQR
 
 class QrListView(LoginRequiredMixin, ListView):
@@ -14,3 +16,32 @@ def renovar_qr_view(request, pk):
     qr.renovar()
     messages.success(request, 'Código QR renovado con éxito.')
     return redirect('qr:lista')
+
+def validar_acceso_view(request):
+    token = request.GET.get('token') # Recibimos el UUID por la URL
+    resultado = None
+    
+    if token:
+        try:
+            # Buscamos el QR por el contenido (UUID)
+            qr = CodigoQR.objects.get(contenido=token)
+            
+            # Validamos reglas de negocio
+            if qr.activo and qr.fecha_expiracion > timezone.now():
+                resultado = {
+                    'status': 'success',
+                    'mensaje': 'Acceso Autorizado',
+                    'vehiculo': qr.vehiculo
+                }
+            else:
+                resultado = {
+                    'status': 'danger',
+                    'mensaje': 'Código expirado o desactivado',
+                }
+        except CodigoQR.DoesNotExist:
+            resultado = {
+                'status': 'danger',
+                'mensaje': 'Código QR no válido',
+            }
+            
+    return render(request, 'qr/validar.html', {'resultado': resultado})
